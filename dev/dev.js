669b3d7e762f5a7e75dd745b72ded7b93f09b00d
Kennedy Rose <kennedy@kennedyrose.com>
2018-04-10 11:31:55 -0500

    Working example

--- dev/dev.js
+++ dev/dev.js
@@ -1,3 +1,18 @@
-import Boilerplate from '../src/index'
-const obj = Boilerplate()
-console.log(obj)
+import Animation from '../src'
+
+const el = document.createElement('div')
+el.textContent = 'test'
+document.body.appendChild(el)
+
+let animation = new Animation({
+	from: {
+		left: 0
+	},
+	to: {
+		left: 50
+	},
+	onStep: ({ left }) => {
+		el.style.transform = `translateX(${left}px)`
+	}
+})
+animation.start()
--- src/index.js
+++ src/index.js
@@ -1,13 +1,74 @@
+import clone from 'clone'
+import rafLoop from 'raf-loop'
 
-console.log('Module loaded')
-
-export default function(){
-	const def = {
-		test: '123'
+class Animate{
+	constructor(options){
+		options = {
+			...defaultOptions,
+			...options,
+		}
+		for(let i in options){
+			this[i] = options[i]
+		}
+		this.engine = rafLoop(this.step.bind(this))
 	}
-	const obj = {
-		anotherTest: 'abc',
-		...def,
+	start(){
+		if(typeof this.from === 'string'){
+			this.state = this.from
+		}
+		else{
+			this.state = clone(this.from)
+		}
+		this.engine.start()
 	}
-	return obj
+	stop(){
+		this.engine.stop()
+	}
+	step(delta){
+		this.time += delta
+		if(this.time >= this.duration){
+			this.end()
+			return
+		}
+		this.deepTween(this.state, this.from, this.to)
+		this.onStep(this.state)
+	}
+	end() {
+		this.state = clone(this.to)
+		this.stop()
+		this.onEnd()
+	}
+	reset(){
+		this.stop()
+		this.time = 0
+		this.state = clone(this.from)
+	}
+	tweenNumber(from, to){
+		return from + this.easing(this.time / this.duration) * to - from
+	}
+	deepTween(state, from, to){
+		for(let i in from){
+			if (typeof from[i] === 'number'){
+				state[i] = this.tweenNumber(from[i], to[i])
+			}
+			else{
+				this.deepTween(state[i], from[i], to[i])
+			}
+		}
+	}
 }
+
+const defaultOptions = {
+	from: 0,
+	to: 100,
+	duration: 1000,
+	time: 0,
+	easing: t => t,
+	onStart: noop,
+	onStep: noop,
+	onEnd: noop,
+}
+
+function noop(){}
+
+export default Animate

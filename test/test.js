20e225f7383f2d4dd577de8cad91d2769f2946fd
Kennedy Rose <kennedy@kennedyrose.com>
2018-04-10 15:07:47 -0500

    Test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,12 @@
 import { expect } from 'chai'
-import theModule from '../src'
+import Animate from '../src'
 
-describe('Default module', () => {
-	it('Should have content', () => {
-		const testVar = theModule()
-		expect(testVar).to.not.be.empty
+describe('Animate class', () => {
+	it('Should have state', () => {
+		const anim = new Animate({
+			from: 100,
+			to: 200
+		})
+		expect(anim.state).to.equal(100)
 	})
 })

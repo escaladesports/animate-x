89a627b02c89fe468bc474d1dfecb5c0023538e6
Kennedy Rose <kennedy@kennedyrose.com>
2018-04-11 09:18:44 -0500

    More tests

--- test/test.js
+++ test/test.js
@@ -3,10 +3,48 @@ import Animate from '../src'
 
 describe('Animate class', () => {
 	it('Should have state', () => {
-		const anim = new Animate({
+		let anim = new Animate({
 			from: 100,
-			to: 200
+			to: 200,
 		})
 		expect(anim.state).to.equal(100)
+	})
+	it('Should increment values', done => {
+		let anim = new Animate({
+				from: 1,
+				to: 100,
+				duration: 1000,
+			})
+			.start()
+		setTimeout(() => {
+			expect(anim.state).to.be.above(1)
+			expect(anim.state).to.be.below(100)
+			done()
+		}, 200)
+	})
+	it('Should reduce values', done => {
+		let anim = new Animate({
+				from: 1,
+				to: -100,
+				duration: 1000,
+			})
+			.start()
+		setTimeout(() => {
+			expect(anim.state).to.be.above(-100)
+			expect(anim.state).to.be.below(1)
+			done()
+		}, 200)
+	})
+	it('Should not go above end value', done => {
+		let anim = new Animate({
+				from: -50,
+				to: 50,
+				duration: 100,
+			})
+			.start()
+		setTimeout(() => {
+			expect(anim.state).to.equal(50)
+			done()
+		}, 200)
 	})
 })
